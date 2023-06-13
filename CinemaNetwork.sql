60ca8d827555ae3a0f2ab69cc5869a413095e2b2
SebastianK2000 <sebastian.kosciolek2000@gmail.com>
2023-06-13 21:25:04 +0200

    create new table

--- CinemaNetwork.sql
+++ CinemaNetwork.sql
@@ -1,9 +1,1 @@
-CREATE TABLE Cinemas (
-	id SERIAL PRIMARY KEY,
-	name_cinema varchar(30) NOT NULL,
-	city varchar(60) NOT NULL,
-	street varchar(100),
-	post_key char(6) NOT NULL, 
-	date_open DATE NOT NULL,
-	date_close DATE
-);
+CREATE DATABASE CinemaNetwork;
